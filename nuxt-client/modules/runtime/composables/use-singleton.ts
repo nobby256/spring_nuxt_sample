62aa6e337bc228d516ae6949aa5719f72641c55f
nobby256 <helloworld_1024@yahoo.co.jp>
2025-11-27 21:47:02 +0900

    更新

--- nuxt-client/modules/runtime/composables/use-singleton.ts
+++ nuxt-client/modules/runtime/composables/use-singleton.ts
@@ -7,21 +7,20 @@
 // --- Public Types ---
 // モジュールの外から使われることが想定される型
 export interface FooOptions {
-  value1?: string;
-  value2?: string;
+  value1?: string
+  value2?: string
 }
 
 export interface Foo {
-  doSomething(): string;
+  doSomething(): string
 }
 
 // --- Private Types ---
 // このモジュールの内部実装でのみ使用される型
 // (例えば、公開するSingletonよりも多くのプロパティを持つ内部的なインスタンスの型)
 interface InternalFoo extends Foo {
-  _resolvedOptions: Required<FooOptions>; // 内部的に保持する設定値
+  _resolvedOptions: Required<FooOptions> // 内部的に保持する設定値
 }
-
 
 // ========================================================================
 // 2. 内部状態と定数 (Internal State & Constants)
@@ -29,43 +28,42 @@ interface InternalFoo extends Foo {
 const defaultOptions: Required<FooOptions> = {
   value1: 'hello',
   value2: 'world',
-};
+}
 
 // インスタンス変数の型注釈には、内部用の`InternalSingleton`を使用できる
-let instance: InternalFoo | undefined;
-
+let instance: InternalFoo | undefined
 
 // ========================================================================
 // 3. 公開API (Public API)
 // ========================================================================
 export const setupUseFoo = (options?: FooOptions): Foo => { // 戻り値は公開用の`Foo`
   if (instance) {
-    throw new Error('Foo has already been created. `setupUseFoo` should only be called once.');
+    throw new Error('Foo has already been created. `setupUseFoo` should only be called once.')
   }
 
   const resolvedOptions: Required<FooOptions> = {
     ...defaultOptions,
     ...(options ?? {}),
-  };
+  }
 
   const doSomething = (): string => {
-    return (resolvedOptions.value1 ?? '') + (resolvedOptions.value2 ?? '');
-  };
+    return (resolvedOptions.value1 ?? '') + (resolvedOptions.value2 ?? '')
+  }
 
   // 実際のインスタンスは`InternalSingleton`の形で作られる
   instance = {
     doSomething,
     _resolvedOptions: resolvedOptions,
-  };
+  }
 
   // ただし、戻り値として返す際は、公開用の`Singleton`として扱われるため、
   // 内部プロパティ(_resolvedOptions)は外部からは見えない。
-  return instance;
-};
+  return instance
+}
 
 export const useFoo = (): Foo => {
   if (!instance) {
-    throw new Error('Foo has not been created yet. Call `setuoUseFoo` in a Nuxt plugin first.');
+    throw new Error('Foo has not been created yet. Call `setuoUseFoo` in a Nuxt plugin first.')
   }
-  return instance;
-};
+  return instance
+}
