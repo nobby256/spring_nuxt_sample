858f430d784ff3a969493a3a20838b5d200db40e
nobby256 <helloworld_1024@yahoo.co.jp>
2025-12-15 20:58:38 +0900

    更新

--- demo-library/src/main/java/com/example/demo/library/errors/DefaultDomainProblem.java
+++ demo-library/src/main/java/com/example/demo/library/errors/DefaultDomainProblem.java
@@ -1,5 +1,6 @@
 package com.example.demo.library.errors;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,9 +9,16 @@ import org.jspecify.annotations.Nullable;
 
 public class DefaultDomainProblem implements DomainProblem<DomainError> {
 
+    public static final URI TYPE = URI.create("/domain-problem");
+
     private @Nullable String detail;
 
     private List<DomainError> errors = new ArrayList<>();
+
+    @Override
+    public URI getType() {
+        return TYPE;
+    }
 
     @Override
     public @Nullable String getDetail() {
